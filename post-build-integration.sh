0cb959f0f3e71baf07827f804d50d735b346dc36
Ayeshasaifsofy <ayesha@sofy.ai>
2023-11-28 21:30:18 +0500

    fixed: changed subscription key to x-sofy-auth-key

--- post-build-integration.sh
+++ post-build-integration.sh
@@ -5,7 +5,7 @@ APPLICATION_GUID="Your application guid"
 CERTIFICATE_NAME="Your application certificate name"
 
 curl --location --request POST 'https://public.sofy.ai/parser-microservice/build-upload' \
---header "SubscriptionKey: $SUBSCRIPTION_KEY" \
+--header "x-sofy-auth-key: $SUBSCRIPTION_KEY" \
 --form "applicationFile=@"$BUILD_PATH"" \
 --form "ApplicationGUID= $APPLICATION_GUID" \
 --form "CertificateName= $CERTIFICATE_NAME"
